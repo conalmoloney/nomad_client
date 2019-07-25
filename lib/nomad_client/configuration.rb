27fc71e78498667d8f06bedfda6ca70691509864
Conal Moloney <conal.moloney@bigcommerce.com>
2019-07-25 11:23:29 -0500

    Set Faraday retry options, add fields on config object for these

--- lib/nomad_client/configuration.rb
+++ lib/nomad_client/configuration.rb
@@ -16,11 +16,29 @@ module NomadClient
     DEFAULT_PORT          = 4646.freeze
     DEFAULT_API_BASE_PATH = '/v1'.freeze
     DEFAULT_SSL           = {}.freeze
+    TIMEOUT               = 5
+    OPEN_TIMEOUT          = 2
+    POOL_SIZE             = 5
+    IDLE_TIMEOUT          = 100
+    RETRY_CHANGE_REQUESTS = true
+    RETRY_MAX             = 3
+    RETRY_INTERVAL        = 1.0
+    RETRY_RANDOMNESS      = 0.5
+    RETRY_BACKOFF_FACTOR  = 2.0
 
     def initialize
-      @port          = DEFAULT_PORT
-      @api_base_path = DEFAULT_API_BASE_PATH
-      @ssl           = DEFAULT_SSL
+      @port                  = DEFAULT_PORT
+      @api_base_path         = DEFAULT_API_BASE_PATH
+      @ssl                   = DEFAULT_SSL
+      @timeout               = TIMEOUT
+      @open_timeout          = OPEN_TIMEOUT
+      @pool_size             = POOL_SIZE
+      @idle_timeout          = IDLE_TIMEOUT
+      @retry_change_requests = RETRY_CHANGE_REQUESTS
+      @retry_max             = RETRY_MAX
+      @retry_interval        = RETRY_INTERVAL
+      @retry_randomness      = RETRY_RANDOMNESS
+      @retry_backoff_factor  = RETRY_BACKOFF_FACTOR
     end
   end
 end
